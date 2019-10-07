7498a8a37d2fdde3c3f65f70b9a57dfffe984f2e
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-10-07 12:42:52 +0200

    Add folders: Controllers, Models, Views with Layout and classes BookController and Book

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+  public class Book
+  {
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+  }
+}
