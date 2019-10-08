dc1209e414c6f39fafe93736b982884c0c109f09
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-10-08 14:14:16 +0200

    Add navigation bar using bootstrap

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -8,5 +8,9 @@ namespace BookStore.Controllers
 {
   public class BookController : Controller
   {
+    public IActionResult Index()
+    {
+      return View();
+    }
   }
 }
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace BookStore
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddMvc(options => options.EnableEndpointRouting = false);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,20 @@ namespace BookStore
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseRouting();
+      app.UseStaticFiles();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapGet("/", async context =>
-              {
-            await context.Response.WriteAsync("Hello World!");
-          });
-      });
+      //app.UseMvcWithDefaultRoute();
+      app.UseMvc(route => route.MapRoute("default", "{controller=Book}/{action=Index}/{id?}"));
+
+      //app.UseRouting();
+
+      //app.UseEndpoints(endpoints =>
+      //{
+      //  endpoints.MapGet("/", async context =>
+      //        {
+      //      await context.Response.WriteAsync("Hello World!");
+      //    });
+      //});
     }
   }
 }
