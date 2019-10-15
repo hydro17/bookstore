3ea80bdfcc24676ca2d9853b52ebb1987ae29d3a
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-10-15 08:09:22 +0200

    add seedBooksTable (one item), add data for next 4 seed items

---
+++ BookStore/Migrations/20191015055011_SeedBooksTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class SeedBooksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "PhotoUniqueName", "Price", "Title" },
+                values: new object[] { 1, "Andrzej Sapkowski", null, "3d4cdb83-8fb8-42aa-a05c-fb2f7d5d1e6d_wiedzmin-miecz-przeznaczenia.jpg", 28.99m, "Wiedźmin Miecz przeznaczenia" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
--- BookStore/Models/AppDbContext.cs
+++ BookStore/Models/AppDbContext.cs
@@ -13,17 +13,50 @@ namespace BookStore.Models
 
     public DbSet<Book> Books { get; set; }
 
-    //protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //{
-    //  modelBuilder.Entity<Book>().HasData(
-    //    new Book
-    //    {
-    //      Title = "Wiedźmin Miecz przeznaczenia",
-    //      Author = "Andrzej Sapkowski",
-    //      Price = 28.99m,
-    //      PhotoUniqueName = "3d4cdb83-8fb8-42aa-a05c-fb2f7d5d1e6d_wiedzmin-miecz-przeznaczenia.jpg"
-    //    }
-    //  );
-    //}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Book>().HasData(
+        new Book
+        {
+          Id = 1,
+          Title = "Wiedźmin Miecz przeznaczenia",
+          Author = "Andrzej Sapkowski",
+          Price = 28.99m,
+          PhotoUniqueName = "3d4cdb83-8fb8-42aa-a05c-fb2f7d5d1e6d_wiedzmin-miecz-przeznaczenia.jpg"
+        },
+        new Book
+        {
+          Id = 2,
+          Title = "Kongres futurologiczny",
+          Author = "Stanisław Lem",
+          Price = 18.89m,
+          PhotoUniqueName = "lem-kongres-futurologiczny.jpg"
+        },
+        new Book
+        {
+          Id = 3,
+          Title = "Czerwona księga",
+          Author = "Carl Gustav Jung",
+          Price = 87.99m,
+          PhotoUniqueName = "czerwona-ksiega.jpg"
+        },
+        new Book
+        {
+          Id = 4,
+          Title = "Dywizjon 303. Walka i codzienność",
+          Author = "Richard King",
+          Price = 25.69m,
+          PhotoUniqueName = "dywizjon-303.jpg"
+        },
+        new Book
+        {
+          Id = 5,
+          Title = "Historia bez cenzury 4",
+          Author = "Wojciech Drewniak",
+          Price = 28.99m,
+          PhotoUniqueName = "historia-bez-cenzury.jpg"
+        }
+      );
+    }
   }
 }
