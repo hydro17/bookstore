48c8b31b7801d55a48dd000285f5aa7dec1c2ec2
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-10-20 19:00:41 +0200

    add repository pattern (interface and mock implementation) for OrderItem

---
+++ BookStore/Models/OrderItems/IOrderItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.OrderItems
+{
+    public interface IOrderItemRepository
+    {
+        Task<IEnumerable<OrderItem>> GetAllAsync();
+        Task<OrderItem> GetByIdAsync(int id);
+        Task<OrderItem> AddAsync(OrderItem orderItem);
+        Task<OrderItem> UpdateAsync(OrderItem orderItemChandes);
+        Task<OrderItem> DeleteAsync(int id);
+    }
+}
--- BookStore/Models/OrderItems/MockOrderItemRepository.cs
+++ BookStore/Models/OrderItems/MockOrderItemRepository.cs
@@ -6,44 +6,78 @@ using System.Threading.Tasks;
 
 namespace BookStore.Models.OrderItems
 {
-    public class MockOrderItemRepository : IOrderItem
+    public class MockOrderItemRepository : IOrderItemRepository
     {
         private IList<OrderItem> _orderItemList;
 
         public MockOrderItemRepository()
         {
-            //_orderItemList = new List<OrderItem>
-            //{
-            //    new OrderItem { 
-            //        Id = 1, 
-            //        Book = new Book { Id  } 
-            //    },
-            //};
+            var _mockBookRepository = new MockBookRepository();
+
+            _orderItemList = new List<OrderItem>
+            {
+                new OrderItem {
+                    Id = 1,
+                    Book = _mockBookRepository.GetById(1),
+                    Quantity = 1
+                },
+                new OrderItem
+                {
+                    Id = 2,
+                    Book = _mockBookRepository.GetById(2),
+                    Quantity = 5
+                },
+                new OrderItem
+                {
+                    Id = 3,
+                    Book = _mockBookRepository.GetById(3),
+                    Quantity = 2
+                }
+            };
         }
 
-        public OrderItem Add(OrderItem orderItem)
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task<OrderItem> AddAsync(OrderItem orderItem)
         {
-            throw new NotImplementedException();
+            if (orderItem != null)
+            {
+                orderItem.Id = _orderItemList.Max(o => o.Id) + 1;
+                _orderItemList.Add(orderItem);
+            }
+
+            return orderItem;
         }
 
-        public OrderItem Delete(int id)
+        public async Task<OrderItem> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            OrderItem orderItem = _orderItemList.FirstOrDefault(o => o.Id == id);
+
+            if (orderItem != null)
+            {
+                _orderItemList.Remove(orderItem);
+            }
+
+            return orderItem;
         }
 
-        public IEnumerable<OrderItem> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IEnumerable<OrderItem>> GetAllAsync() => _orderItemList;
 
-        public OrderItem GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<OrderItem> GetByIdAsync(int id) => _orderItemList.FirstOrDefault(o => o.Id == id);
 
-        public OrderItem Update(OrderItem orderItemChandes)
+        public async Task<OrderItem> UpdateAsync(OrderItem orderItemChanges)
         {
-            throw new NotImplementedException();
+            if (orderItemChanges == null) throw new ArgumentNullException(nameof(orderItemChanges));
+
+            OrderItem orderItem = _orderItemList.FirstOrDefault(o => o.Id == orderItemChanges.Id);
+
+            if (orderItem != null)
+            {
+                orderItem.Book = orderItemChanges.Book;
+                orderItem.Quantity = orderItemChanges.Quantity;
+            }
+
+            return orderItem;
         }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
     }
 }
